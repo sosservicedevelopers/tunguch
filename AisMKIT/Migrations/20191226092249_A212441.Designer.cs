db196499d4f86a3e34f2a2df754b17296ede6495
eseptehelp <eseptehelp@gmail.com>
2019-12-26 12:31:29 +0300

    CRUD-операции:
    DictLocs
    DictTypeOfKMMs
    DictInitialProjs
    
    ListOfCultEvents

--- AisMKIT/Migrations/20191226092249_A212441.Designer.cs
+++ AisMKIT/Migrations/20191226092249_A212441.Designer.cs
@@ -4,14 +4,16 @@ using AisMKIT.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace AisMKIT.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20191226092249_A212441")]
+    partial class A212441
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ AisMKIT/Models/CultEvents.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace AisMKIT.Models
+{
+
+    //public class ListOfEvents
+    //{
+    //    public int Id { get; set; }
+
+    //    [Display(Name = "Тип МКК")]
+    //    public int DictTypeOfKMMId { get; set; }
+    //    public DictTypeOfKMM DictTypeOfKMM { get; set; }
+
+    //    [Display(Name = "Место проведения")]
+    //    public int DistLocId { get; set; }
+    //    [Display(Name = "Место проведения")]
+    //    public DictLoc DistLoc { get; set; }
+
+    //    [Display(Name = "Тема мероприятия")]
+    //    public string EventTopic { get; set; } //Тема мероприятия
+
+    //    [Display(Name = "Дата и время начало")]
+    //    public string StartDateTime { get; set; } //дата и время начала
+
+    //    [Display(Name = "Дата и время конец")]
+    //    public string EndDateTime { get; set; } //дата и время конец
+
+    //    [Display(Name = "Инициатор проекта")]
+    //    public int DictInitiatorOfProjId { get; set; }
+    //    [Display(Name = "Инициатор проекта")]
+    //    public DictInitiatorOfProj DictInitiatorOfProj { get; set; }
+    //}
+
+
+    //public class DictTypeOfKMM
+    //{
+    //    public int Id { get; set; }
+    //    [Display(Name = "Тип МКК")]
+    //    public string Name { get; set; }
+    //}
+    
+    //public class DictLoc
+    //{
+    //    public int Id { get; set; }
+    //    [Display(Name = "Место проведения")]
+    //    public string Name { get; set; }
+    //}
+    //public class DictInitiatorOfProj
+    //{
+    //    public int Id { get; set; }
+    //    [Display(Name = "Инициатор проекта")]
+    //    public string Name { get; set; }
+    //}
+
+}
