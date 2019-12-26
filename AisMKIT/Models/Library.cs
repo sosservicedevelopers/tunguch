a4c12eeabb66bb119f127b2ef6fa9e06799db619
eseptehelp <eseptehelp@gmail.com>
2019-12-26 05:34:46 +0300

    Обновление модели показатели библиотек

--- AisMKIT/Models/Library.cs
+++ AisMKIT/Models/Library.cs
@@ -10,61 +10,61 @@ namespace AisMKIT.Models
     {
         public int Id { get; set; }
 
-        [Display(Name = "Наименов б - ки")]
+        [Display(Name = "Наименование библиотеки")]
         public string LibraryName { get; set; } //название библиотеки
 
         [Display(Name = "Адресные данные БГЦБС")]
         public string AddressData { get; set; }// Адресные данные БГЦБС
 
-        [Display(Name = "общ площ м кв")]
+        [Display(Name = "Общая площадь м.кв.")]
         public float TotalArea { get; set; }//общ пл. м кв
 
-        [Display(Name = "посад места")]
+        [Display(Name = "Посадочные места")]
         public string SeatLanding { get; set; }//посад места
 
-        [Display(Name = "авар капит Б - ка")]
+        [Display(Name = "Аварийная капитальная библиотека")]
         public string EmerCapLib { get; set; }// авар капит библиотека
 
-        [Display(Name = "спец присп Б - ка")]
+        [Display(Name = "Специально-приспособленная библиотека")]
         public string SpecAdapLib { get; set; }//специальная приспособленная библиотека
 
-        [Display(Name = "Произв.капит ремонт")]
+        [Display(Name = "Произведен капитальный ремонт")]
         public string OverhaulMade { get; set; }//произведен капитальный ремонт
 
-        [Display(Name = "Произв. космет ремонт")]
+        [Display(Name = "Произведен косметический ремонт")]
         public string Redecorated { get; set; }//произведен косметический ремонт
 
-        [Display(Name = "компьютеры /множит. шт")]
+        [Display(Name = "Компьютеры /множит. штук")]
         public int Computers { get; set; }//компьютеры
 
-        [Display(Name = "Подкл.к интернету")]
+        [Display(Name = "Подключение к интернету")]
         public int InternetConnection { get; set; }//подключение к интернету
 
-        [Display(Name = "Комп. Для пользоват")]
+        [Display(Name = "Компьютер для пользователей")]
         public int ComputersForUsers { get; set; }//компьютеры для пользователей
 
-        [Display(Name = "Подкл. для польз к интерн")]
+        [Display(Name = "Подключение для пользователей к интернету")]
         public int UserConnection { get; set; }//подключение пользователей к интернету
 
-        [Display(Name = "Пользователи чел")]
+        [Display(Name = "Пользователи количество человек")]
         public int UsersLib { get; set; }//пользователи
 
-        [Display(Name = "Поступило/ выбыло, всего")]
+        [Display(Name = "Поступило/выбыло  всего")]
         public int RecRetTotal { get; set; }//поступило, выбыло всего
 
-        [Display(Name = "всего док экз")]
+        [Display(Name = "Всего документы экземпляры")]
         public int TotalNumOfEx { get; set; }//всего док экз    
 
-        [Display(Name = "на кырг яз экз")]
+        [Display(Name = "На кыргызском языке экземпляры")]
         public int CopKyrg { get; set; }//экземпляры на кыргызском
 
         [Display(Name = "Мероприятий, всего")]
         public int EventsLib { get; set; }//мероприятия
 
-        [Display(Name = "всегоб - рей чел")]
+        [Display(Name = "Всего библиотекарей")]
         public int Librarians { get; set; }//библиотекари
 
-        [Display(Name = "Образ  чел")]
+        [Display(Name = "Образование человека")]
         public string DegEducation { get; set; }//образ чел
 
         public string ApplicationUserId { get; set; }
